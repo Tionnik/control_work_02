bc58c46fef08886eceb4ac5cd698f8a9ffd9a360
Nik <ice_fire50@mail.ru>
2023-07-19 17:30:16 +0300

    end code

--- Product.java
+++ Product.java
@@ -1,16 +1,25 @@
 package control_work_02;
 
-public class Product {
+public class Product implements Comparable <Product> {
     
     private int id;
     private String name;
     private int count;
+    private int chance;
+
+    public Product (int vendor_code, String name_product, int count, int chance){
+        this.id  = vendor_code;
+        this.name = name_product;
+        this.count = count;
+        this.chance = chance;
+    }
 
     public Product (int vendor_code, String name_product, int count){
         this.id  = vendor_code;
         this.name = name_product;
         this.count = count;
     }
+
     public int getId() {
         return id;
     }
@@ -19,5 +28,20 @@ public class Product {
     }
     public int getCount() {
         return count;
+    }
+    public int getChance() {
+        return chance;
+    }
+
+    @Override
+    public int compareTo(Product other) {
+        if (other == null) {
+            return -1; 
+        }
+        int delta = this.chance - other.chance;
+        if (delta != 0) {
+            return - delta;
+        }  
+        return this.name.compareTo(other.name);
     }
 }
--- main.java
+++ main.java
@@ -1,7 +1,48 @@
 package control_work_02;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
 public class main {
-    public static void main(String[] args) {
-        
+
+    public static void main(String[] args) throws IOException {
+        ArrayList <Product> catalog =new ArrayList<>();
+        catalog.add(new Product(17, "Doll", 6));
+        catalog.add(new Product(21, "constructor", 5));
+        catalog.add(new Product(7, "soccer ball", 2));
+        catalog.add(new Product(27, "basketball ball", 2));
+        catalog.add(new Product(5, "badminton set", 0));
+        catalog.add(new Product(14, "table tennis set", 3));
+    
+        PriorityQueue <Product> list_draw = new PriorityQueue<>();      
+        for (Product position : catalog) {
+            int count = position.getCount();
+           while (count > 0) {
+                int id = (int) (Math.random() * 900 + 100);
+                System.out.println(id);
+                list_draw.add(new Product(position.getId(),position.getName(),position.getCount(), id ));
+                count --;
+           }
+        }
+        if ( ! Files.isRegularFile(Path.of("C:\\New\\dz\\control_work_02\\result.txt"))){
+            Files.createFile(Path.of("C:\\New\\dz\\control_work_02\\result.txt"));
+        }
+        else {
+            Files.delete(Path.of("C:\\New\\dz\\control_work_02\\result.txt"));
+            Files.createFile(Path.of("C:\\New\\dz\\control_work_02\\result.txt"));
+        }
+        Product element;
+        while((element = list_draw.poll())!= null) { 
+            System.out.println(element.getId() +", "+ element.getName());
+            Path path = Paths.get("C:\\New\\dz\\control_work_02\\result.txt");
+            String text = element.getId()+","+element.getName()+","+element.getChance()+"\n";
+            Files.write(path, text.getBytes(), StandardOpenOption.APPEND);
+        }
+
     }
 }
