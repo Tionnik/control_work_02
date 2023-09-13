7eda55863c7936852c86378952a4fbf440d56662
Nik <ice_fire50@mail.ru>
2023-09-13 20:10:52 +0300

    end control wokr 2

--- Core.java
+++ Core.java
@@ -1,54 +1,83 @@
 package control_work_02;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 
 public class Core {
     
-    ArrayList <Product> catalog = new ArrayList<>(); 
+    private ArrayList <Product> catalog = new ArrayList<>();                    // поле с листом разыгрываемыми товарами 
+    private PriorityQueue <Product> list_draw = new PriorityQueue<>();          //поле с товарами в приоритетной очереди 
     
-    public void addProduct(String text){
+    public void addProduct(String text){                                        // Метод добавления товара в лист с разыгрываемыми товарами
         String [] prod = text.split(";");
         int count = Integer.parseInt(prod[2]);
         catalog.add(new Product(prod[0], prod[1], count));
     }
 
-    public void getCatalog() {
+    public void getCatalog() {                                                  // Показывает все товары в листе товаров
         for (Product product : catalog) {
             System.out.println(product.getId()+" "+product.getName()+" "+product.getCount());
         }
     }
-        /*
-        ArrayList <Product> catalog =new ArrayList<>();                                                     // Создание и заполнение листа с игрушками для розыгрыша
-        catalog.add(new Product(17, "Doll", 6));
-        catalog.add(new Product(21, "constructor", 5));
-        catalog.add(new Product(7, "soccer ball", 2));
-        catalog.add(new Product(27, "basketball ball", 2));
-        catalog.add(new Product(5, "badminton set", 0));
-        catalog.add(new Product(14, "table tennis set", 3));
-    
-        PriorityQueue <Product> list_draw = new PriorityQueue<>();                                          // Создание приоритетной очереди 
-        for (Product position : catalog) {
+
+    public void draw(){                                                                                     // Создание приоритетной очереди
+        for (Product position : catalog) {                                                                  // Переберает весь лист товаров
             int count = position.getCount();                                                                // где к каждому предмету
-           while (count > 0) {
+            while (count > 0) {
                 int id = (int) (Math.random() * 900 + 100);                                                 // добавляется случайное трех значное число
-                //System.out.println(id);
-                list_draw.add(new Product(position.getId(),position.getName(),position.getCount(), id ));
+                list_draw.add(new Product(position.getId(),position.getName(),position.getCount(), id ));   // и вносится товар в приоритетную очередь 
                 count --;
            }
         }
-        if ( ! Files.isRegularFile(Path.of("result.txt"))){               // если файла нет, он его создаст
-            Files.createFile(Path.of("result.txt"));
+    }
+
+    public void file(){                                                             // Подготовит файл для отчета по розыгрышу
+        try {
+            if ( ! Files.isRegularFile(Path.of("result.txt"))){               // если файла нет, он его создаст
+                Files.createFile(Path.of("result.txt"));}
+            else {
+                Files.delete(Path.of("result.txt"));                          // иначе удалит текущий
+                Files.createFile(Path.of("result.txt"));}                     // и создаст новый
+        } catch (Exception e) {
+            System.out.println("Ошибра при работе с файлом: "+e.getMessage());      // покажет ошибку при работе с файлом
         }
-        else {
-            Files.delete(Path.of("result.txt"));                          // иначе удалит текущий и создаст новый
-            Files.createFile(Path.of("result.txt"));
-        }
+    }
+
+    public void endDraw(){                                                                                  // допишет весь не разыгранный товар в файл
         Product element;
-        while((element = list_draw.poll())!= null) {                                                        // пока в приоритетной очереди есть элементы, изымается из нее с самым большим добавочным числом.
-            System.out.println(element.getId() +", "+ element.getName());                                   // Показывает его артикул и название.
-            Path path = Paths.get("result.txt");
-            String text = element.getId()+","+element.getName()+","+element.getChance()+"\n";               // Записывает в файл артикул игрушки, 
-            Files.write(path, text.getBytes(), StandardOpenOption.APPEND);                                  //её название и номер шанса выпадения
+        Path path = Paths.get("result.txt");
+        try {                                                                                               // допишет в файл строку разделения разыгранного и не разыгранного товара
+            String text0 = "\nНе разыгранный товар \n";
+            Files.write(path, text0.getBytes(), StandardOpenOption.APPEND);
+        } catch (Exception e) {
+            System.out.println("Запись в файл была с ошибкой: "+e.getMessage());
         }
-        */
+        while((element = list_draw.poll())!= null) {                                                        // пока в приоритетной очереди есть элементы, изымается из нее с самым большим добавочным числом.
+            String text = element.getId()+","+element.getName()+","+element.getChance()+"\n";               // Записывает в файл артикул игрушки, её название и номер шанса выпадения
+            try {
+                Files.write(path, text.getBytes(), StandardOpenOption.APPEND);                              
+            } catch (Exception e) {
+                System.out.println("Запись в файл была с ошибкой: "+e.getMessage());
+            } 
+        } 
+    }
+
+    public String getProduct(){                                                                             // Разыгрывает позицию и записывает её в файл 
+        Product element = list_draw.poll();
+        if(element!= null){
+            Path path = Paths.get("result.txt");
+            String text = element.getId()+","+element.getName()+","+element.getChance()+"\n";               // Записывает в файл артикул игрушки,
+            try {
+                Files.write(path, text.getBytes(), StandardOpenOption.APPEND);                              //её название и номер шанса выпадения
+                return (element.getId() +", "+ element.getName());
+            } catch (Exception e) {
+                return ("Запись в файл была с ошибкой: "+e.getMessage());
+            } 
+        }
+        else { return "Спасибо за розыгрыш\n"; }
+    }
 }
--- View.java
+++ View.java
@@ -11,49 +11,70 @@ public class View {
     public void start(){
         Core core = new Core();
         while (true) {
-            System.out.println();
+            System.out.println();                                                   // интерфейсное меню 
             System.out.println("[1] Добавить товар к розыгрышу");
             System.out.println("[2] Показать список разыгрываемых товаров");
             System.out.println("[3] Перейти к розыгрышу");
             System.out.println("[0] выйти из программы");
             System.out.print("Выберите команду для работы: ");
-            command = scan.nextInt();
+            command = scan.nextInt();                                               // Запрос команды от пользователя
             scan.nextLine();
 
-            if (command == 1){
+            if (command == 1){                                  // Запрашивает правильную строку с товаром от пользователя и добавляет товар в список розыгрыша.
                  core.addProduct(addName());
                  System.out.println("Товар успешно добавлен в список розыгрыша");
             }
-            if (command == 2){
+
+            if (command == 2){                                  // Запрашивает весь введённый список с товарами для розыгрыша 
                 System.out.println();
                 core.getCatalog();
             }
-            if (command == 3){
-                break;
+
+            if (command == 3){                                  // Запускает розыгрыш
+                core.file();                                    // подготавливает файл для отчета
+                core.draw();                                    // Создает и заполняет приоритетную очередь товарами и их весами
+                System.out.println();
+                System.out.println("Нажмите любую цифру для розыгрыша следующего предмета");
+                System.out.println("[0] для завершения розыгрыша");
+                while (true){
+                    command = scan.nextInt();                   // запрашивает команду для розыгрыша 
+                    scan.nextLine();
+                    if (command == 0){                          //если команда 0,  
+                        core.endDraw();                         //записывает весь оставшийся товар в файл
+                        break; }                                //и завершает розыгрыш
+                    else {
+                        String text = core.getProduct();        // производит одиночный розыгрыш
+                        System.out.println(text);               // показывает выигранный товар
+                        if(text.equals("Спасибо за розыгрыш\n")){    //если товаров нет
+                            break;                              // завершает розыгрыш
+                        }
+                    }
+                }
+                break;                                          // Завершает работу всей программы
             }
-            if (command == 0){
+
+            if (command == 0){                                  // Завершает работу всей программы
                 break;
             }
         }
         scan.close();
     }
 
-    private String addName(){
+    private String addName(){                                                                 // Метод запроса разыгрываемого продукта от пользователя  
         while (true){
             System.out.println("Введите разыгрываемый товар в виде:\n Артикул товара; Название товара; количество товара");
             text = scan.nextLine();
-            String [] test = text.split(";");
+            String [] test = text.split(";");                                           // Проверят правильность формата ввода
             if (test.length == 3){
                 try {
-                    Integer.parseInt(test[2]);
-                    if (( ! test[0].equals(""))&&( ! test[1].equals(""))){
+                    Integer.parseInt(test[2]);                                                // Проверят, что кол-во товара введено цифрой  
+                    if (( ! test[0].equals(""))&&( ! test[1].equals(""))){  // Проверят, что артикул и название не пустое
                         return text;
                     }
                     else {System.out.println("Не заполнено Артикул или название товара");}
                 } catch (Exception e) {
                     System.out.println("На позиции числа, должно быть целое число");
                 }
-                
             }
             else {System.out.println("Формат ввода неверен");}
         }
